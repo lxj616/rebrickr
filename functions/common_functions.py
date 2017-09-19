de521226206e368f457d1cfc9b837cfd0980b130
Christopher Gearhart <cgear13@gmail.com>
2017-09-19 03:13:34 -0400

    Fixed typo

--- functions/common_functions.py
+++ functions/common_functions.py
@@ -313,7 +313,7 @@ def writeErrorToFile(errorReportPath, txtName, addonVersion):
         os.makedirs(errorReportPath)
     fullFilePath = os.path.join(errorReportPath, "Brickinator_error_report.txt")
     f = open(fullFilePath, "w")
-    f.write("\nPlease copy the following form and paste it into a new issue at https://github.com/bblanimation/Brickinator/issues")
+    f.write("\nPlease copy the following form and paste it into a new issue at https://github.com/bblanimation/brickinator/issues")
     f.write("\n\nDon't forget to include a description of your problem! The more information you provide (what you were trying to do, what action directly preceeded the error, etc.), the easier it will be for us to squash the bug.")
     f.write("\n\n### COPY EVERYTHING BELOW THIS LINE ###\n")
     f.write("\nDescription of the Problem:\n")
