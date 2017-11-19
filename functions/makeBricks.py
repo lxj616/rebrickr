lxj616/rebrickr

commit 533fd85350f410efcfe9a6f56c46b1ab70f81aa6

Various brick operations now update automatically; also laid foundations for creating unique brick type meshes

functions/makeBricks.py
@@ -186,7 +186,7 @@ def getClosestMaterial(cm, bricksD, key, brickType, randState, brick_mats, k):
 
 
 @timed_call('Time Elapsed')
-def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customData=None, customObj_details=None, group_name=None, frameNum=None, cursorStatus=False):
+def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customData=None, customObj_details=None, group_name=None, frameNum=None, cursorStatus=False, keys="ALL", createGroup=True):
     # set up variables
     scn = bpy.context.scene
     cm = scn.cmlist[scn.cmlist_index]
@@ -205,8 +205,9 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
     # apply transformation to logo duplicate and get bounds(logo)
     logo_details, logo = prepareLogoAndGetDetails(logo)
 
-    # get brick dicts in seeded order
-    keys = list(bricksD.keys())
+    # get bricksD dicts in seeded order
+    if keys == "ALL":
+        keys = list(bricksD.keys())
     keys.sort()
     random.seed(cm.mergeSeed)
     random.shuffle(keys)
@@ -214,13 +215,17 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
     keys.sort(key=lambda x: int(x.split(",")[2]))
 
     # create group for bricks
-    if group_name:
+    if group_name is not None:
         Rebrickr_bricks = group_name
     else:
         Rebrickr_bricks = 'Rebrickr_%(n)s_bricks' % locals()
-    if groupExists(Rebrickr_bricks):
-        bpy.data.groups.remove(group=bpy.data.groups[Rebrickr_bricks], do_unlink=True)
-    bGroup = bpy.data.groups.new(Rebrickr_bricks)
+    if createGroup:
+        if groupExists(Rebrickr_bricks):
+            bpy.data.groups.remove(group=bpy.data.groups[Rebrickr_bricks], do_unlink=True)
+        bGroup = bpy.data.groups.new(Rebrickr_bricks)
+    else:
+        bGroup = bpy.data.groups.get(Rebrickr_bricks)
+
 
     tempMesh = bpy.data.meshes.new("tempMesh")
 
@@ -461,9 +466,10 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
     if cursorStatus:
         wm.progress_end()
 
+    bricksCreated = []
     # combine meshes, link to scene, and add relevant data to the new Blender MESH object
     if split:
-        for i,key in enumerate(bricksD):
+        for i,key in enumerate(keys):
             # print status to terminal
             percent = i/len(bricksD)
             if percent < 1:
@@ -483,6 +489,7 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
                 brick.parent = parent
                 scn.objects.link(brick)
                 brick.isBrick = True
+                bricksCreated.append(brick)
         update_progress("Linking to Scene", 1)
     else:
         m = combineMeshes(allBrickMeshes)
@@ -514,7 +521,10 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
         scn.objects.link(allBricksObj)
         # protect allBricksObj from being deleted
         allBricksObj.isBrickifiedObject = True
+        bricksCreated.append(allBricksObj)
 
     # reset 'attempted_merge' for all items in bricksD
-    for key0 in bricksD:
+    for key0 in keys:
         bricksD[key0]["attempted_merge"] = False
+
+    return bricksCreated

buttons/brickify.py
@@ -140,7 +140,7 @@ class RebrickrBrickify(bpy.types.Operator):
         self.createdGroups = []
         scn = bpy.context.scene
         cm = scn.cmlist[scn.cmlist_index]
-        self.setAction(scn, cm)
+        self.action = self.getAction(cm)
         self.sourceOrig = self.getObjectToBrickify()
 
     def getObjectToBrickify(self):
@@ -163,6 +163,17 @@ class RebrickrBrickify(bpy.types.Operator):
         safeScn = getSafeScn()
         safeScn.objects.link(parent)
         return parent
+
+    @classmethod
+    def getLogo(cls, cm):
+        if cm.brickType != "Custom":
+            if cm.logoDetail == "LEGO Logo":
+                refLogo = cls.getLegoLogo(cls)
+            else:
+                refLogo = bpy.data.objects.get(cm.logoObjectName)
+        else:
+            refLogo = None
+        return refLogo
 
     def getLegoLogo(self):
         scn = bpy.context.scene
@@ -198,8 +209,18 @@ class RebrickrBrickify(bpy.types.Operator):
 
         return refLogo
 
+    def runCreateNewBricks(self, source, parent, source_details, dimensions, refLogo, action, curFrame=None):
+        group_name = self.createNewBricks(source, parent, source_details, dimensions, refLogo, action, curFrame=curFrame)
+        if int(round((source_details.x.distance)/(dimensions["width"]+dimensions["gap"]))) == 0:
+            self.report({"WARNING"}, "Model is too small on X axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
+        if int(round((source_details.y.distance)/(dimensions["width"]+dimensions["gap"]))) == 0:
+            self.report({"WARNING"}, "Model is too small on Y axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
+        if int(round((source_details.z.distance)/(dimensions["height"]+dimensions["gap"]))) == 0:
+            self.report({"WARNING"}, "Model is too small on Z axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
+        return group_name
 
-    def createNewBricks(self, source, parent, source_details, dimensions, refLogo, curFrame=None):
+    @classmethod
+    def createNewBricks(cls, source, parent, source_details, dimensions, refLogo, action, curFrame=None, bricksDict=None, keys="ALL", createGroup=True, selectCreated=False):
         scn = bpy.context.scene
         cm = scn.cmlist[scn.cmlist_index]
         n = cm.source_name
@@ -222,39 +243,41 @@ class RebrickrBrickify(bpy.types.Operator):
             customData = None
             customObj_details = None
             R = (dimensions["width"]+dimensions["gap"], dimensions["width"]+dimensions["gap"], dimensions["height"]+dimensions["gap"])
-        updateCursor = self.action in ["CREATE", "UPDATE_MODEL"] # evaluates to boolean value
-        bricksDict, loadedFromCache = getBricksDict(self.action, source, source_details, dimensions, R, updateCursor, curFrame)
+        updateCursor = action in ["CREATE", "UPDATE_MODEL"] # evaluates to boolean value
+        if bricksDict is None:
+            bricksDict, loadedFromCache = getBricksDict(action, source, source_details, dimensions, R, updateCursor, curFrame)
         if curFrame is not None:
             group_name = 'Rebrickr_%(n)s_bricks_frame_%(curFrame)s' % locals()
         else:
             group_name = None
-        if self.action == "UPDATE_MODEL" and cm.buildIsDirty and loadedFromCache:
+        # reset all values for certain keys in bricksDict dictionaries
+        if action == "UPDATE_MODEL" and cm.buildIsDirty and loadedFromCache:
             for kk in bricksDict:
                 bD = bricksDict[kk]
                 bD["size"] = None
                 bD["parent_brick"] = None
                 bD["top_exposed"] = None
                 bD["bot_exposed"] = None
-        makeBricks(parent, refLogo, dimensions, bricksDict, cm.splitModel, R=R, customData=customData, customObj_details=customObj_details, group_name=group_name, frameNum=curFrame, cursorStatus=updateCursor)
-        cacheBricksDict(self.action, cm, bricksDict) # store current bricksDict to cache
-        if int(round((source_details.x.distance)/(dimensions["width"]+dimensions["gap"]))) == 0:
-            self.report({"WARNING"}, "Model is too small on X axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
-        if int(round((source_details.y.distance)/(dimensions["width"]+dimensions["gap"]))) == 0:
-            self.report({"WARNING"}, "Model is too small on Y axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
-        if int(round((source_details.z.distance)/(dimensions["height"]+dimensions["gap"]))) == 0:
-            self.report({"WARNING"}, "Model is too small on Z axis for an accurate calculation. Try scaling up your model or decreasing the brick size for a more accurate calculation.")
+        bricksCreated = makeBricks(parent, refLogo, dimensions, bricksDict, cm.splitModel, R=R, customData=customData, customObj_details=customObj_details, group_name=group_name, frameNum=curFrame, cursorStatus=updateCursor, keys=keys, createGroup=createGroup)
+        if selectCreated:
+            for brick in bricksCreated:
+                brick.select = True 
+        cacheBricksDict(action, cm, bricksDict) # store current bricksDict to cache
         return group_name
 
-    def setAction(self, scn, cm):
-        """ sets self.action """
+    @classmethod
+    def getAction(cls, cm):
+        """ returns action """
         if cm.modelCreated:
-            self.action = "UPDATE_MODEL"
+            action = "UPDATE_MODEL"
         elif cm.animated:
-            self.action = "UPDATE_ANIM"
+            action = "UPDATE_ANIM"
         elif not cm.useAnimation:
-            self.action = "CREATE"
+            action = "CREATE"
         else:
-            self.action = "ANIMATE"
+            action = "ANIMATE"
+
+        return action
 
     def isValid(self, source, Rebrickr_bricks_gn):
         """ returns True if brickify action can run, else report WARNING/ERROR and return False """
@@ -623,13 +646,7 @@ class RebrickrBrickify(bpy.types.Operator):
         self.createdObjects.append(parent0.name)
 
         # update refLogo
-        if cm.brickType != "Custom":
-            if cm.logoDetail == "LEGO Logo":
-                refLogo = self.getLegoLogo()
-            else:
-                refLogo = bpy.data.objects.get(cm.logoObjectName)
-        else:
-            refLogo = None
+        refLogo = self.getLogo(cm)
 
         # begin drawing status to cursor
         wm = bpy.context.window_manager
@@ -672,7 +689,7 @@ class RebrickrBrickify(bpy.types.Operator):
 
             # create new bricks
             try:
-                group_name = self.createNewBricks(source, parent, source_details, dimensions, refLogo, curFrame=curFrame)
+                group_name = self.runCreateNewBricks(source, parent, source_details, dimensions, refLogo, self.action, curFrame=curFrame)
                 self.createdGroups.append(group_name)
             except KeyboardInterrupt:
                 self.report({"WARNING"}, "Process forcably interrupted with 'KeyboardInterrupt'")
@@ -861,16 +878,10 @@ class RebrickrBrickify(bpy.types.Operator):
         self.createdObjects.append(parent.name)
 
         # update refLogo
-        if cm.brickType != "Custom":
-            if cm.logoDetail == "LEGO Logo":
-                refLogo = self.getLegoLogo()
-            else:
-                refLogo = bpy.data.objects.get(cm.logoObjectName)
-        else:
-            refLogo = None
+        refLogo = self.getLogo(cm)
 
         # create new bricks
-        self.createNewBricks(source, parent, source_details, dimensions, refLogo)
+        self.runCreateNewBricks(source, parent, source_details, dimensions, refLogo, self.action)
 
         bGroup = bpy.data.groups.get(Rebrickr_bricks_gn) # redefine bGroup since it was removed
         if bGroup is not None:

functions/common_functions.py
@@ -247,6 +247,7 @@ def select(objList=[], active=None, deselect=False, only=True, scene=None):
     return True
 
 def delete(objList):
+    if objList is None: return False
     objList = confirmList(objList)
     objs = bpy.data.objects
     for obj in objList:

lib/Brick/cylinder_mesh_generate.py
@@ -3,190 +3,7 @@ import bmesh
 import math
 from mathutils import Matrix
 
-# r = radius, N = numVerts, h = height, co = target cylinder position, botFace = Bool for creating face on bottom, bme = bmesh to insert mesh data into
-def makeCylinder(r, N, h, co=(0,0,0), botFace=True, topFace=True, bme=None):
-    # create new bmesh object
-    if bme == None:
-        bme = bmesh.new()
-
-    # create upper and lower circles
-    vertListT = []
-    vertListB = []
-    sideFaces = []
-    for i in range(N):
-        x = r * math.cos(((2 * math.pi) / N) * i)
-        y = r * math.sin(((2 * math.pi) / N) * i)
-        z = h / 2
-        coordT = (x + co[0], y + co[1], z + co[2])
-        coordB = (x + co[0], y + co[1], -z + co[2])
-        vertListT.append(bme.verts.new(coordT))
-        vertListB.append(bme.verts.new(coordB))
-
-    # create top and bottom faces
-    topVerts = vertListT[:]
-    botVerts = vertListB[::-1]
-    if topFace:
-        bme.faces.new(topVerts)
-    if botFace:
-        bme.faces.new(botVerts)
-
-    # create faces on the sides
-    sideFaces.append(bme.faces.new((vertListT[-1], vertListB[-1], vertListB[0], vertListT[0])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListT.pop(0), vertListB.pop(0), vertListB[0], vertListT[0])))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    # return bmesh
-    return bme, botVerts, topVerts
-
-# r = radius, N = numVerts, h = height, t = thickness, co = target cylinder position
-def makeTube(r, N, h, t, co=(0,0,0), wings=False, bme=None):
-    # create new bmesh object
-    if bme == None:
-        bme = bmesh.new()
-
-    # create upper and lower circles
-    vertListTInner = []
-    vertListBInner = []
-    vertListTOuter = []
-    vertListBOuter = []
-    for i in range(N):
-        # set coord x,y,z locations
-        xInner = r * math.cos(((2 * math.pi) / N) * i)
-        xOuter = (r + t) * math.cos(((2 * math.pi) / N) * i)
-        yInner = r * math.sin(((2 * math.pi) / N) * i)
-        yOuter = (r + t) * math.sin(((2 * math.pi) / N) * i)
-        z = h / 2
-        # inner cylinder verts
-        coordT = (xInner + co[0], yInner + co[1], z + co[2])
-        coordB = (xInner + co[0], yInner + co[1], -z + co[2])
-        vertListTInner.append(bme.verts.new(coordT))
-        vertListBInner.append(bme.verts.new(coordB))
-        # outer cylinder verts
-        coordT = (xOuter + co[0], yOuter + co[1], z + co[2])
-        coordB = (xOuter + co[0], yOuter + co[1], -z + co[2])
-        vertListTOuter.append(bme.verts.new(coordT))
-        vertListBOuter.append(bme.verts.new(coordB))
-        # create faces between them
-        if i > 0:
-            bme.faces.new((vertListBOuter[-2], vertListBInner[-2], vertListBInner[-1], vertListBOuter[-1]))
-    # select top verts for exclusion from vert group
-    for lst in [vertListTOuter, vertListTInner]:
-        for v in lst:
-            v.select = True
-
-    bme.faces.new((vertListBOuter[-1], vertListBInner[-1], vertListBInner[0], vertListBOuter[0]))
-
-    # create faces on the outer and inner sides
-    sideFaces = []
-    sideFaces.append(bme.faces.new((vertListTOuter[-1], vertListBOuter[-1], vertListBOuter[0], vertListTOuter[0])))
-    sideFaces.append(bme.faces.new((vertListTInner[0], vertListBInner[0], vertListBInner[-1], vertListTInner[-1])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListTOuter.pop(0), vertListBOuter.pop(0), vertListBOuter[0], vertListTOuter[0])))
-        sideFaces.append(bme.faces.new((vertListTInner[1], vertListBInner[1], vertListBInner.pop(0), vertListTInner.pop(0))))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    # return bmesh
-    return bme
-
-# r = radius, N = numVerts, h = height, o = z offset, co = target cylinder position, bme = bmesh to insert mesh data into
-def makeInnerCylinder(r, N, h, co=(0,0,0), bme=None):
-    """ Make a brick inner cylinder """
-    # create upper circle
-    vertListT = []
-    vertListB = []
-    vertListBDict = {"++":[], "-+":[], "--":[], "+-":[], "x+":[], "x-":[], "y+":[], "y-":[]}
-    for i in range(N):
-        # set coord x,y,z locations
-        x = r * math.cos(((2 * math.pi) / N) * i)
-        y = r * math.sin(((2 * math.pi) / N) * i)
-        z = co[2]
-        # create top verts
-        vertListT.append(bme.verts.new((x + co[0], y + co[1], z + h)))
-
-        # create bottom verts and add to dict
-        v = bme.verts.new((x + co[0], y + co[1], z))
-        yP = v.co.y > co[1] # true if 'y' is positive
-        xP = v.co.x > co[0] # true if 'x' is positive
-        if abs(v.co.x - co[0]) < 0.00001:
-            if yP:
-                l = "y+"
-            else:
-                l = "y-"
-        elif abs(v.co.y - co[1]) < 0.00001:
-            if xP:
-                l = "x+"
-            else:
-                l = "x-"
-        else:
-            if xP and yP:
-                l = "++"
-            elif not xP and yP:
-                l = "-+"
-            elif not xP and not yP:
-                l = "--"
-            else:
-                l = "+-"
-        vertListBDict[l].insert(0,v)
-        vertListB.append(v)
-    bme.faces.new(vertListT[::-1])
-    if len(vertListBDict["y+"]) == 0:
-        v0 = vertListBDict["++"][0]
-        v1 = vertListBDict["-+"][0]
-        if v0.co.y > v1.co.y:
-            vertListBDict["y+"] = [vertListBDict["++"].pop(0)]
-        else:
-            vertListBDict["y+"] = [vertListBDict["-+"].pop(-1)]
-    if len(vertListBDict["x+"]) == 0:
-        v0 = vertListBDict["+-"][0]
-        v1 = vertListBDict["++"][0]
-        if v0.co.x > v1.co.x:
-            vertListBDict["x+"] = [vertListBDict["+-"].pop(0)]
-        else:
-            vertListBDict["x+"] = [vertListBDict["++"].pop(-1)]
-    if len(vertListBDict["y-"]) == 0:
-        v0 = vertListBDict["--"][0]
-        v1 = vertListBDict["+-"][0]
-        if v0.co.y > v1.co.y:
-            vertListBDict["y-"] = [vertListBDict["--"].pop(0)]
-        else:
-            vertListBDict["y-"] = [vertListBDict["+-"].pop(-1)]
-    if len(vertListBDict["x-"]) == 0:
-        v0 = vertListBDict["-+"][0]
-        v1 = vertListBDict["--"][0]
-        if v0.co.x > v1.co.x:
-            vertListBDict["x-"] = [vertListBDict["-+"].pop(0)]
-        else:
-            vertListBDict["x-"] = [vertListBDict["--"].pop(-1)]
-
-    # select bottom verts for vertex group
-    for v in vertListB:
-        v.select = True
-
-#    # create lower circle faces with square
-#    lastKey = "x-y"
-#    for key in ["xy", "-xy", "-x-y", "x-y"]:
-#        bme.faces.new((vertListBDict[lastKey][1][-1], vertListBDict[key][1][0], vertListBDict[key][0], vertListBDict[lastKey][0]))
-#        for i in range(1, len(vertListBDict[key][1])):
-#            bme.faces.new((vertListBDict[key][1][i-1], vertListBDict[key][1][i], vertListBDict[key][0]))
-#        lastKey = key
-
-    # create faces around edge
-    sideFaces = []
-    sideFaces.append(bme.faces.new((vertListT[0], vertListB[0], vertListB[-1], vertListT[-1])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListT[1], vertListB[1], vertListB.pop(0), vertListT.pop(0))))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    return vertListBDict
-
-def makeBrick(dimensions, brickSize, numStudVerts=None, detail="Low Detail", logo=None, stud=True, bme=None):
+def makeCylinder(dimensions, brickSize, numStudVerts=None, detail="Low Detail", logo=None, stud=True, bme=None):
     # create new bmesh object
     if not bme:
         bme = bmesh.new()

lib/Brick/slope_inverted_mesh_generate.py
@@ -3,190 +3,7 @@ import bmesh
 import math
 from mathutils import Matrix
 
-# r = radius, N = numVerts, h = height, co = target cylinder position, botFace = Bool for creating face on bottom, bme = bmesh to insert mesh data into
-def makeCylinder(r, N, h, co=(0,0,0), botFace=True, topFace=True, bme=None):
-    # create new bmesh object
-    if bme == None:
-        bme = bmesh.new()
-
-    # create upper and lower circles
-    vertListT = []
-    vertListB = []
-    sideFaces = []
-    for i in range(N):
-        x = r * math.cos(((2 * math.pi) / N) * i)
-        y = r * math.sin(((2 * math.pi) / N) * i)
-        z = h / 2
-        coordT = (x + co[0], y + co[1], z + co[2])
-        coordB = (x + co[0], y + co[1], -z + co[2])
-        vertListT.append(bme.verts.new(coordT))
-        vertListB.append(bme.verts.new(coordB))
-
-    # create top and bottom faces
-    topVerts = vertListT[:]
-    botVerts = vertListB[::-1]
-    if topFace:
-        bme.faces.new(topVerts)
-    if botFace:
-        bme.faces.new(botVerts)
-
-    # create faces on the sides
-    sideFaces.append(bme.faces.new((vertListT[-1], vertListB[-1], vertListB[0], vertListT[0])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListT.pop(0), vertListB.pop(0), vertListB[0], vertListT[0])))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    # return bmesh
-    return bme, botVerts, topVerts
-
-# r = radius, N = numVerts, h = height, t = thickness, co = target cylinder position
-def makeTube(r, N, h, t, co=(0,0,0), wings=False, bme=None):
-    # create new bmesh object
-    if bme == None:
-        bme = bmesh.new()
-
-    # create upper and lower circles
-    vertListTInner = []
-    vertListBInner = []
-    vertListTOuter = []
-    vertListBOuter = []
-    for i in range(N):
-        # set coord x,y,z locations
-        xInner = r * math.cos(((2 * math.pi) / N) * i)
-        xOuter = (r + t) * math.cos(((2 * math.pi) / N) * i)
-        yInner = r * math.sin(((2 * math.pi) / N) * i)
-        yOuter = (r + t) * math.sin(((2 * math.pi) / N) * i)
-        z = h / 2
-        # inner cylinder verts
-        coordT = (xInner + co[0], yInner + co[1], z + co[2])
-        coordB = (xInner + co[0], yInner + co[1], -z + co[2])
-        vertListTInner.append(bme.verts.new(coordT))
-        vertListBInner.append(bme.verts.new(coordB))
-        # outer cylinder verts
-        coordT = (xOuter + co[0], yOuter + co[1], z + co[2])
-        coordB = (xOuter + co[0], yOuter + co[1], -z + co[2])
-        vertListTOuter.append(bme.verts.new(coordT))
-        vertListBOuter.append(bme.verts.new(coordB))
-        # create faces between them
-        if i > 0:
-            bme.faces.new((vertListBOuter[-2], vertListBInner[-2], vertListBInner[-1], vertListBOuter[-1]))
-    # select top verts for exclusion from vert group
-    for lst in [vertListTOuter, vertListTInner]:
-        for v in lst:
-            v.select = True
-
-    bme.faces.new((vertListBOuter[-1], vertListBInner[-1], vertListBInner[0], vertListBOuter[0]))
-
-    # create faces on the outer and inner sides
-    sideFaces = []
-    sideFaces.append(bme.faces.new((vertListTOuter[-1], vertListBOuter[-1], vertListBOuter[0], vertListTOuter[0])))
-    sideFaces.append(bme.faces.new((vertListTInner[0], vertListBInner[0], vertListBInner[-1], vertListTInner[-1])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListTOuter.pop(0), vertListBOuter.pop(0), vertListBOuter[0], vertListTOuter[0])))
-        sideFaces.append(bme.faces.new((vertListTInner[1], vertListBInner[1], vertListBInner.pop(0), vertListTInner.pop(0))))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    # return bmesh
-    return bme
-
-# r = radius, N = numVerts, h = height, o = z offset, co = target cylinder position, bme = bmesh to insert mesh data into
-def makeInnerCylinder(r, N, h, co=(0,0,0), bme=None):
-    """ Make a brick inner cylinder """
-    # create upper circle
-    vertListT = []
-    vertListB = []
-    vertListBDict = {"++":[], "-+":[], "--":[], "+-":[], "x+":[], "x-":[], "y+":[], "y-":[]}
-    for i in range(N):
-        # set coord x,y,z locations
-        x = r * math.cos(((2 * math.pi) / N) * i)
-        y = r * math.sin(((2 * math.pi) / N) * i)
-        z = co[2]
-        # create top verts
-        vertListT.append(bme.verts.new((x + co[0], y + co[1], z + h)))
-
-        # create bottom verts and add to dict
-        v = bme.verts.new((x + co[0], y + co[1], z))
-        yP = v.co.y > co[1] # true if 'y' is positive
-        xP = v.co.x > co[0] # true if 'x' is positive
-        if abs(v.co.x - co[0]) < 0.00001:
-            if yP:
-                l = "y+"
-            else:
-                l = "y-"
-        elif abs(v.co.y - co[1]) < 0.00001:
-            if xP:
-                l = "x+"
-            else:
-                l = "x-"
-        else:
-            if xP and yP:
-                l = "++"
-            elif not xP and yP:
-                l = "-+"
-            elif not xP and not yP:
-                l = "--"
-            else:
-                l = "+-"
-        vertListBDict[l].insert(0,v)
-        vertListB.append(v)
-    bme.faces.new(vertListT[::-1])
-    if len(vertListBDict["y+"]) == 0:
-        v0 = vertListBDict["++"][0]
-        v1 = vertListBDict["-+"][0]
-        if v0.co.y > v1.co.y:
-            vertListBDict["y+"] = [vertListBDict["++"].pop(0)]
-        else:
-            vertListBDict["y+"] = [vertListBDict["-+"].pop(-1)]
-    if len(vertListBDict["x+"]) == 0:
-        v0 = vertListBDict["+-"][0]
-        v1 = vertListBDict["++"][0]
-        if v0.co.x > v1.co.x:
-            vertListBDict["x+"] = [vertListBDict["+-"].pop(0)]
-        else:
-            vertListBDict["x+"] = [vertListBDict["++"].pop(-1)]
-    if len(vertListBDict["y-"]) == 0:
-        v0 = vertListBDict["--"][0]
-        v1 = vertListBDict["+-"][0]
-        if v0.co.y > v1.co.y:
-            vertListBDict["y-"] = [vertListBDict["--"].pop(0)]
-        else:
-            vertListBDict["y-"] = [vertListBDict["+-"].pop(-1)]
-    if len(vertListBDict["x-"]) == 0:
-        v0 = vertListBDict["-+"][0]
-        v1 = vertListBDict["--"][0]
-        if v0.co.x > v1.co.x:
-            vertListBDict["x-"] = [vertListBDict["-+"].pop(0)]
-        else:
-            vertListBDict["x-"] = [vertListBDict["--"].pop(-1)]
-
-    # select bottom verts for vertex group
-    for v in vertListB:
-        v.select = True
-
-#    # create lower circle faces with square
-#    lastKey = "x-y"
-#    for key in ["xy", "-xy", "-x-y", "x-y"]:
-#        bme.faces.new((vertListBDict[lastKey][1][-1], vertListBDict[key][1][0], vertListBDict[key][0], vertListBDict[lastKey][0]))
-#        for i in range(1, len(vertListBDict[key][1])):
-#            bme.faces.new((vertListBDict[key][1][i-1], vertListBDict[key][1][i], vertListBDict[key][0]))
-#        lastKey = key
-
-    # create faces around edge
-    sideFaces = []
-    sideFaces.append(bme.faces.new((vertListT[0], vertListB[0], vertListB[-1], vertListT[-1])))
-    for v in range(N-1):
-        sideFaces.append(bme.faces.new((vertListT[1], vertListB[1], vertListB.pop(0), vertListT.pop(0))))
-
-    for f in sideFaces:
-        f.smooth = True
-
-    return vertListBDict
-
-def makeBrick(dimensions, brickSize, numStudVerts=None, detail="Low Detail", logo=None, stud=True, bme=None):
+def makeSlopeInverted(dimensions, brickSize, numStudVerts=None, detail="Low Detail", logo=None, stud=True, bme=None):
     # create new bmesh object
     if not bme:
         bme = bmesh.new()

commit d81bbd55baa7bb718723f496268b923925eaa27e

Fixed error with generating 'Bricks and Plates' model introduced in commit https://github.com/bblanimation/rebrickr/commit/533fd85350f410efcfe9a6f56c46b1ab70f81aa6

functions/makeBricks.py
@@ -273,14 +273,15 @@ def makeBricks(parent, logo, dimensions, bricksD, split=False, R=None, customDat
         # if second time through this for loop, go through keys left behind if "Bricks and Plates"
         if timeThrough == 2: # second time
             if cm.brickType == "Bricks and Plates":
-                keys = keysLeftBehind
+                keys0 = keysLeftBehind
             else:
                 break
         else: # first time
             keysLeftBehind = []
+            keys0 = keys
 
         # iterate through locations in bricksD from bottom to top
-        for i,key in enumerate(keys):
+        for i,key in enumerate(keys0):
             brickD = bricksD[key]
             if brickD["draw"] and brickD["parent_brick"] in [None, "self"]:
 