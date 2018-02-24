2efdb3c0c063c1990e363635f8afc05923c16c52
“bblanimation” <cgear13@gmail.com>
2018-02-24 03:29:46 -0500

    Re-added changes lost in commit 6a11ecc

--- lib/Brick/get_brick_dimensions.py
+++ lib/Brick/get_brick_dimensions.py
@@ -8,22 +8,25 @@ def get_brick_dimensions(height=1, zScale=1, gap_percentage=0.01):
     zScale         -- height of the brick in plates (1: standard plate, 3: standard brick)
     gap_percentage -- gap between bricks relative to brick height
     """
+
     scale = height/9.6
     brick_dimensions = {}
-    brick_dimensions["height"] = scale*9.6*(zScale/3)
-    brick_dimensions["width"] = scale*8
-    brick_dimensions["gap"] = scale*9.6*gap_percentage
-    brick_dimensions["stud_height"] = scale*1.8
-    brick_dimensions["stud_radius"] = scale*2.4
-    brick_dimensions["stud_offset"] = (brick_dimensions["height"] / 2) + (brick_dimensions["stud_height"] / 2)
-    brick_dimensions["thickness"] = scale*1.6
-    brick_dimensions["tube_thickness"] = scale*0.855
-    brick_dimensions["bar_radius"] = scale*1.6
-    brick_dimensions["logo_width"] = scale*3.74
-    brick_dimensions["support_width"] = scale*0.8
-    brick_dimensions["tick_width"] = scale*0.6
-    brick_dimensions["tick_depth"] = scale*0.3
-    brick_dimensions["support_height"] = brick_dimensions["height"]*0.65
+    brick_dimensions["height"] = round(scale*9.6*(zScale/3), 8)
+    brick_dimensions["width"] = round(scale*8, 8)
+    brick_dimensions["gap"] = round(scale*9.6*gap_percentage, 8)
+    brick_dimensions["stud_height"] = round(scale*1.8, 8)
+    brick_dimensions["stud_radius"] = round(scale*2.4, 8)
+    brick_dimensions["stud_offset"] = round((brick_dimensions["height"] / 2) + (brick_dimensions["stud_height"] / 2), 8)
+    brick_dimensions["stud_offset_triple"] = round(((brick_dimensions["height"]*3) / 2) + (brick_dimensions["stud_height"] / 2), 8)
+    brick_dimensions["thickness"] = round(scale*1.6, 8)
+    brick_dimensions["tube_thickness"] = round(scale*0.855, 8)
+    brick_dimensions["bar_radius"] = round(scale*1.6, 8)
+    brick_dimensions["logo_width"] = round(scale*4.8, 8) # originally round(scale*3.74, 8)
+    brick_dimensions["support_width"] = round(scale*0.8, 8)
+    brick_dimensions["tick_width"] = round(scale*0.6, 8)
+    brick_dimensions["tick_depth"] = round(scale*0.3, 8)
+    brick_dimensions["support_height"] = round(brick_dimensions["height"]*0.65, 8)
+    brick_dimensions["support_height_triple"] = round((brick_dimensions["height"]*3)*0.65, 8)
 
-    brick_dimensions["logo_offset"] = (brick_dimensions["height"] / 2) + (brick_dimensions["stud_height"])
+    brick_dimensions["logo_offset"] = round((brick_dimensions["height"] / 2) + (brick_dimensions["stud_height"]), 8)
     return brick_dimensions
