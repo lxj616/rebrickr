lxj616/rebrickr

commit 64ccf6f234dcd45b06bba0b44c44ec1311057757

Refactored and simplified code

buttons/customize/tools/mergeBricks.py
@@ -119,35 +119,28 @@ class mergeBricks(Operator):
     # class methods
 
     @staticmethod
-    def getSortedKeys(keys):
-        """ sort bricks by (x+y) location for best merge """
-        keys.sort(key=lambda k: (strToList(k)[0] * strToList(k)[1] * strToList(k)[2]))
-        return keys
-
-    @staticmethod
     def mergeBricks(bricksDict, keys, cm, mergeVertical=True, targetType="BRICK", height3Only=False):
         # initialize vars
         updatedKeys = []
         zStep = getZStep(cm)
         randState = np.random.RandomState(cm.mergeSeed)
 
-        keys = mergeBricks.getSortedKeys(keys)
+        # remove child bricks from keys list
+        keys = [k for k in keys if bricksDict[k]["parent_brick"] in [None, "self"]]
+        # sort keys
+        keys.sort(key=lambda k: (strToList(k)[0] * strToList(k)[1] * strToList(k)[2]))
 
         for key in keys:
-            if bricksDict[key]["parent_brick"] in [None, "self"]:
-                # attempt to merge current brick with other bricks in keys, according to available brick types
-                # TODO: improve originalIsBrick argument (currently hardcoded to False)
-                loc = strToList(key)
-                parentD = bricksDict[getParentKey(bricksDict, key)]
-                tallType = targetType if targetType in getBrickTypes(height=3) else parentD["type"]
-                shortType = targetType if targetType in getBrickTypes(height=1) else (parentD["type"] if parentD["type"] in getBrickTypes(height=1) else "PLATE")
-                brickSize = attemptMerge(cm, bricksDict, key, keys, loc, [parentD["size"]], zStep, randState, preferLargest=True, mergeVertical=mergeVertical, shortType=shortType, tallType=tallType, height3Only=height3Only)
-                # bricksDict[key]["size"] = brickSize
-                # set exposure of current [merged] brick
-                topExposed, botExposed = getBrickExposure(cm, bricksDict, key, loc)
-                bricksDict[key]["top_exposed"] = topExposed
-                bricksDict[key]["bot_exposed"] = botExposed
-                updatedKeys.append(key)
+            # attempt to merge current brick with other bricks in keys, according to available brick types
+            parentD = bricksDict[getParentKey(bricksDict, key)]
+            tallType = targetType if targetType in getBrickTypes(height=3) else parentD["type"]
+            shortType = targetType if targetType in getBrickTypes(height=1) else (parentD["type"] if parentD["type"] in getBrickTypes(height=1) else "PLATE")
+            brickSize = attemptMerge(cm, bricksDict, key, keys, [parentD["size"]], zStep, randState, preferLargest=True, mergeVertical=mergeVertical, shortType=shortType, tallType=tallType, height3Only=height3Only)
+            # set exposure of current [merged] brick
+            topExposed, botExposed = getBrickExposure(cm, bricksDict, key)
+            bricksDict[key]["top_exposed"] = topExposed
+            bricksDict[key]["bot_exposed"] = botExposed
+            updatedKeys.append(key)
         return updatedKeys
 
     #############################################

buttons/customize/tools/changeBrickType.py
@@ -98,9 +98,9 @@ class changeBrickType(Operator):
                         bricksDict[dictKey]["rotated"] == self.rotateBrick):
                         # return {"CANCELLED"}
                         continue
-                    # skip bricks that can't be turned into the chosen brick type
-                    elif brickSize[:2] not in legalBrickSizes[3 if self.brickType in getBrickTypes(height=3) else 1][self.brickType]:
-                        continue
+                    # # skip bricks that can't be turned into the chosen brick type
+                    # elif brickSize[:2] not in legalBrickSizes[3 if self.brickType in getBrickTypes(height=3) else 1][self.brickType]:
+                    #     continue
 
                     # verify locations above are not obstructed
                     if self.brickType in getBrickTypes(height=3) and brickSize[2] == 1:

buttons/customize/tools/drawAdjacent.py
@@ -158,52 +158,28 @@ class drawAdjacent(Operator):
             self.bricksDict, _ = getBricksDict(cm=cm)
 
             # initialize direction bools
-            self.zPos = False
-            self.zNeg = False
-            self.yPos = False
-            self.yNeg = False
-            self.xPos = False
-            self.xNeg = False
+            self.zPos, self.zNeg, self.yPos, self.yNeg, self.xPos, self.xNeg = [False] * 6
 
             # initialize vars for self.adjDKLs setup
             dictKey, dictLoc = getDictKey(obj.name)
-            x,y,z = dictLoc
             objSize = self.bricksDict[dictKey]["size"]
-            zStep = getZStep(cm)
+            x,y,z = dictLoc
+            sX, sY, sZ = objSize[0], objSize[1], objSize[2] // getZStep(cm)
             self.adjDKLs = [[],[],[],[],[],[]]
+            # initialize ranges
+            rgs = Vector((range(x, x + sX),
+                          range(y, y + sY),
+                          range(z, z + sZ)))
             # set up self.adjDKLs
-            for y0 in range(y, y + objSize[1]):
-                for z0 in range(z, z + (objSize[2]//zStep)):
-                    dkl = [x + objSize[0], y0, z0]
-                    self.adjDKLs[0].append(dkl)
-            for y0 in range(y, y + objSize[1]):
-                for z0 in range(z, z + (objSize[2]//zStep)):
-                    dkl = [x - 1, y0, z0]
-                    self.adjDKLs[1].append(dkl)
-            for x0 in range(x, x + objSize[0]):
-                for z0 in range(z, z + (objSize[2]//zStep)):
-                    dkl = [x0, y + objSize[1], z0]
-                    self.adjDKLs[2].append(dkl)
-            for x0 in range(x, x + objSize[0]):
-                for z0 in range(z, z + (objSize[2]//zStep)):
-                    dkl = [x0, y - 1, z0]
-                    self.adjDKLs[3].append(dkl)
-            for x0 in range(x, x + objSize[0]):
-                for y0 in range(y, y + objSize[1]):
-                    if cm.brickType == "BRICKS AND PLATES":
-                        dkl = [x0, y0, z + objSize[2]]
-                    else:
-                        dkl = [x0, y0, z + 1]
-                    self.adjDKLs[4].append(dkl)
-            for x0 in range(x, x + objSize[0]):
-                for y0 in range(y, y + objSize[1]):
-                    dkl = [x0, y0, z - 1]
-                    self.adjDKLs[5].append(dkl)
+            self.adjDKLs[0] += [[x + sX, y0, z0] for z0 in rgs.z for y0 in rgs.y]
+            self.adjDKLs[1] += [[x - 1, y0, z0]  for z0 in rgs.z for y0 in rgs.y]
+            self.adjDKLs[2] += [[x0, y + sY, z0] for z0 in rgs.z for x0 in rgs.x]
+            self.adjDKLs[3] += [[x0, y - 1, z0]  for z0 in rgs.z for x0 in rgs.x]
+            self.adjDKLs[4] += [[x0, y0, z + sZ] for y0 in rgs.y for x0 in rgs.x]
+            self.adjDKLs[5] += [[x0, y0, z - 1]  for y0 in rgs.y for x0 in rgs.x]
 
             # initialize self.adjBricksCreated
-            self.adjBricksCreated = []
-            for i in range(6):
-                self.adjBricksCreated.append([False]*len(self.adjDKLs[i]))
+            self.adjBricksCreated = [[False] * len(self.adjDKLs[i]) for i in range(6)]
 
             # initialize self.brickType
             objType = self.bricksDict[dictKey]["type"]
@@ -261,19 +237,11 @@ class drawAdjacent(Operator):
         return newBrickHeight
 
     def getNewCoord(self, cm, co, dimensions, side, newBrickHeight):
-        co = Vector(co)
-        if side == 0:
-            co.x += dimensions["width"]
-        if side == 1:
-            co.x -= dimensions["width"]
-        if side == 2:
-            co.y += dimensions["width"]
-        if side == 3:
-            co.y -= dimensions["width"]
-        if side == 4:
-            co.z += dimensions["height"]
-        if side == 5:
-            co.z -= dimensions["height"] * (newBrickHeight if cm.brickType == "BRICKS AND PLATES" else 1)
+        full_d = [dimensions["width"], dimensions["width"], dimensions["height"]]
+        if side in [0, 2, 4]:  # positive directions
+            co[side//2] += full_d[side//2]
+        else:                  # negative directions
+            co[side//2] -= full_d[side//2] * (newBrickHeight if side == 5 and cm.brickType == "BRICKS AND PLATES" else 1)
         return co.to_tuple()
 
     def isBrickAlreadyCreated(self, brickNum, side):
@@ -293,18 +261,10 @@ class drawAdjacent(Operator):
             cm.numBricksGenerated += 1
             j = cm.numBricksGenerated
             newDictLoc = adjDictLoc.copy()
-            if side == 0:    # X+
-                newDictLoc[0] = newDictLoc[0] - 1
-            elif side == 1:  # X-
-                newDictLoc[0] = newDictLoc[0] + 1
-            elif side == 2:  # Y+
-                newDictLoc[1] = newDictLoc[1] - 1
-            elif side == 3:  # Y-
-                newDictLoc[1] = newDictLoc[1] + 1
-            elif side == 4:  # Z+
-                newDictLoc[2] = newDictLoc[2] - 1
-            elif side == 5:  # Z-
-                newDictLoc[2] = newDictLoc[2] + (newBrickHeight if cm.brickType == "BRICKS AND PLATES" else 1)
+            if side in [0, 2, 4]:  # positive directions
+                newDictLoc[side//2] -= 1
+            else:                  # negative directions
+                newDictLoc[side//2] += (newBrickHeight if side == 5 and cm.brickType == "BRICKS AND PLATES" else 1)
             theKey = listToStr(newDictLoc)
             co0 = self.bricksDict[theKey]["co"]
             co = self.getNewCoord(cm, co0, dimensions, side, newBrickHeight)
@@ -330,64 +290,62 @@ class drawAdjacent(Operator):
                 self.report({"INFO"}, "Brick already exists in the following location: %(adjacent_key)s" % locals())
                 return False
             # if attempting to remove brick
-            else:
-                adjBrickD["draw"] = False
-                setCurBrickVal(self.bricksDict, adjDictLoc, action="REMOVE")
-                adjBrickD["size"] = None
-                adjBrickD["parent_brick"] = None
-                adjBrickD["bot_exposed"] = None
-                adjBrickD["top_exposed"] = None
-                brick = bpy.data.objects.get(adjBrickD["name"])
-                if brick: delete(brick)
-                self.adjBricksCreated[side][brickNum] = False
-                return True
+            adjBrickD["draw"] = False
+            setCurBrickVal(self.bricksDict, adjDictLoc, action="REMOVE")
+            adjBrickD["size"] = None
+            adjBrickD["parent_brick"] = None
+            adjBrickD["bot_exposed"] = None
+            adjBrickD["top_exposed"] = None
+            brick = bpy.data.objects.get(adjBrickD["name"])
+            if brick: delete(brick)
+            self.adjBricksCreated[side][brickNum] = False
+            return True
         # if brick doesn't exist there
         else:
-            # if attempting to add brick
-            if addBrick:
-                if checkTwoMoreAbove:
-                    # check if locs available above current
-                    x0, y0, z0 = adjDictLoc
-                    for z in range(1, 3):
-                        newKey = listToStr([x0, y0, z0 + z])
-                        # if brick drawn in next loc and not just rerunning based on new direction selection
-                        if (newKey in self.bricksDict and self.bricksDict[newKey]["draw"] and
-                            (not self.isBrickAlreadyCreated(brickNum, side) or
-                             self.adjBricksCreated[side][brickNum] not in getBrickTypes(height=3))):
-                            self.report({"INFO"}, "Brick already exists in the following location: %(newKey)s" % locals())
-                            self.setDirBool(side, False)
-                            # reset values at failed location, in case brick was previously drawn there
-                            self.adjBricksCreated[side][brickNum] = False
-                            adjBrickD["draw"] = False
-                            return False
-                        keysToMerge.append(listToStr([x0, y0, z0 + z]))
-                # update dictionary of locations above brick
-                if cm.brickType == "BRICKS AND PLATES":
-                    curType = self.adjBricksCreated[side][brickNum] if self.adjBricksCreated[side][brickNum] else "PLATE"
-                    updateBrickSizeAndDict(dimensions, cm, self.bricksDict, [1, 1, newBrickHeight], adjacent_key, adjDictLoc, curType=curType, targetType=self.brickType)
-                # update dictionary location of adjacent brick created
-                adjBrickD["draw"] = True
-                adjBrickD["type"] = self.brickType
-                adjBrickD["flipped"] = self.bricksDict[dictKey]["flipped"]
-                adjBrickD["rotated"] = self.bricksDict[dictKey]["rotated"]
-                setCurBrickVal(self.bricksDict, strToList(adjacent_key))
-                adjBrickD["mat_name"] = self.bricksDict[dictKey]["mat_name"]
-                adjBrickD["size"] = [1, 1, newBrickHeight]
-                adjBrickD["parent_brick"] = "self"
-                adjBrickD["mat_name"] = self.bricksDict[dictKey]["mat_name"] if adjBrickD["mat_name"] == "" else adjBrickD["mat_name"]
-                adjBrickD["nearest_face"] = self.bricksDict[dictKey]["nearest_face"] if adjBrickD["nearest_face"] is None else adjBrickD["nearest_face"]
-                adjBrickD["nearest_intersection"] = self.bricksDict[dictKey]["nearest_intersection"] if adjBrickD["nearest_intersection"] is None else adjBrickD["nearest_intersection"]
-                topExposed, botExposed = getBrickExposure(cm, self.bricksDict, adjacent_key)
-                adjBrickD["top_exposed"] = topExposed
-                adjBrickD["bot_exposed"] = botExposed
-                keysToMerge.append(adjacent_key)
-                # set adjBricksCreated to target brick type for current side
-                self.adjBricksCreated[side][brickNum] = self.brickType
-
-                return True
             # if attempting to remove brick
-            else:
+            if not addBrick:
                 self.report({"INFO"}, "Brick does not exist in the following location: %(adjacent_key)s" % locals())
                 return False
+            # check if locs above current are available
+            if checkTwoMoreAbove:
+                x0, y0, z0 = adjDictLoc
+                for z in range(1, 3):
+                    newKey = listToStr([x0, y0, z0 + z])
+                    # if brick drawn in next loc and not just rerunning based on new direction selection
+                    if (newKey in self.bricksDict and self.bricksDict[newKey]["draw"] and
+                        (not self.isBrickAlreadyCreated(brickNum, side) or
+                         self.adjBricksCreated[side][brickNum] not in getBrickTypes(height=3))):
+                        self.report({"INFO"}, "Brick already exists in the following location: %(newKey)s" % locals())
+                        self.setDirBool(side, False)
+                        # reset values at failed location, in case brick was previously drawn there
+                        self.adjBricksCreated[side][brickNum] = False
+                        adjBrickD["draw"] = False
+                        return False
+                    else:
+                        keysToMerge.append(newKey)
+            # update dictionary of locations above brick
+            if cm.brickType == "BRICKS AND PLATES":
+                curType = self.adjBricksCreated[side][brickNum] if self.adjBricksCreated[side][brickNum] else "PLATE"
+                updateBrickSizeAndDict(dimensions, cm, self.bricksDict, [1, 1, newBrickHeight], adjacent_key, adjDictLoc, curType=curType, targetType=self.brickType)
+            # update dictionary location of adjacent brick created
+            adjBrickD["draw"] = True
+            adjBrickD["type"] = self.brickType
+            adjBrickD["flipped"] = self.bricksDict[dictKey]["flipped"]
+            adjBrickD["rotated"] = self.bricksDict[dictKey]["rotated"]
+            setCurBrickVal(self.bricksDict, strToList(adjacent_key))
+            adjBrickD["mat_name"] = self.bricksDict[dictKey]["mat_name"]
+            adjBrickD["size"] = [1, 1, newBrickHeight]
+            adjBrickD["parent_brick"] = "self"
+            adjBrickD["mat_name"] = self.bricksDict[dictKey]["mat_name"] if adjBrickD["mat_name"] == "" else adjBrickD["mat_name"]
+            adjBrickD["nearest_face"] = self.bricksDict[dictKey]["nearest_face"] if adjBrickD["nearest_face"] is None else adjBrickD["nearest_face"]
+            adjBrickD["nearest_intersection"] = self.bricksDict[dictKey]["nearest_intersection"] if adjBrickD["nearest_intersection"] is None else adjBrickD["nearest_intersection"]
+            topExposed, botExposed = getBrickExposure(cm, self.bricksDict, adjacent_key)
+            adjBrickD["top_exposed"] = topExposed
+            adjBrickD["bot_exposed"] = botExposed
+            keysToMerge.append(adjacent_key)
+            # set adjBricksCreated to target brick type for current side
+            self.adjBricksCreated[side][brickNum] = self.brickType
+
+            return True
 
     #############################################

functions/makeBricks.py
@@ -124,44 +124,43 @@ def makeBricks(parent, logo, dimensions, bricksDict, cm=None, split=False, brick
         # iterate through locations in bricksDict from bottom to top
         for i, key in enumerate(keys):
             brickD = bricksDict[key]
-            if brickD["draw"] and brickD["parent_brick"] in [None, "self"] and not brickD["attempted_merge"]:
-                # initialize vars
-                loc = strToList(key)
-                brickSizes = [[1, 1, zStep]]
-
-                # if bricks and plates, skip second and third rows on first time through
-                if BandP and cm.alignBricks:
-                    # initialize lowestLoc if not done already
-                    if lowestLoc == -1:
-                        lowestLoc = loc[2]
-                    # check if row should be skipped
-                    if skipThisRow(timeThrough, lowestLoc, loc):
-                        continue
-
-                # merge current brick with available adjacent bricks
-                brickSize = mergeWithAdjacentBricks(cm, brickD, bricksDict, key, keysNotChecked, loc, brickSizes, zStep, randS1)
-                # add brickSize to cm.brickSizesUsed if not already there
-                brickSizeStr = listToStr(sorted(brickSize[:2]) + [brickSize[2]])
-                cm.brickSizesUsed += brickSizeStr if cm.brickSizesUsed == "" else ("|" + brickSizeStr if brickSizeStr not in cm.brickSizesUsed.split("|") else "")
-                cm.brickTypesUsed += brickD["type"] if cm.brickTypesUsed == "" else ("|" + str(brickD["type"]) if brickD["type"] not in cm.brickTypesUsed.split("|") else "")
-
-
-                # create brick based on the current brickD information
-                lastBricksDict = bricksDict
-                drawBrick(cm, bricksDict, brickD, key, loc, keys, i, dimensions, brickSize, split, customData, customObj_details, brickScale, keysNotChecked, bricksCreated, supportBrickDs, allBrickMeshes, logo, logo_details, mats, brick_mats, internalMat, randS1, randS2, randS3, randS4)
-
-                # print build status to terminal
-                old_percent = printBuildStatus(keys, printStatus, cursorStatus, keysNotChecked, old_percent)
-
-                # remove keys in new brick from keysNotChecked (for attemptMerge)
-                updateKeysNotChecked(brickSize, loc, zStep, keysNotChecked, key)
-
-            else:
-                # remove ignored key from keysNotChecked (for attemptMerge)
-                try:
+            # skip keys that are already drawn or have attempted merge
+            if not brickD["draw"] or brickD["parent_brick"] not in [None, "self"] or brickD["attempted_merge"]:
+                # remove ignored keys from keysNotChecked (for attemptMerge)
+                if key in keysNotChecked:
                     keysNotChecked.remove(key)
-                except ValueError:
-                    pass
+                continue
+
+            # initialize vars
+            loc = strToList(key)
+            brickSizes = [[1, 1, zStep]]
+
+            # if bricks and plates, skip second and third rows on first time through
+            if BandP and cm.alignBricks:
+                # initialize lowestLoc if not done already
+                if lowestLoc == -1:
+                    lowestLoc = loc[2]
+                # check if row should be skipped
+                if skipThisRow(timeThrough, lowestLoc, loc):
+                    continue
+
+            # merge current brick with available adjacent bricks
+            brickSize = mergeWithAdjacentBricks(cm, brickD, bricksDict, key, keysNotChecked, brickSizes, zStep, randS1)
+            # add brickSize to cm.brickSizesUsed if not already there
+            brickSizeStr = listToStr(sorted(brickSize[:2]) + [brickSize[2]])
+            cm.brickSizesUsed += brickSizeStr if cm.brickSizesUsed == "" else ("|" + brickSizeStr if brickSizeStr not in cm.brickSizesUsed.split("|") else "")
+            cm.brickTypesUsed += brickD["type"] if cm.brickTypesUsed == "" else ("|" + str(brickD["type"]) if brickD["type"] not in cm.brickTypesUsed.split("|") else "")
+
+
+            # create brick based on the current brickD information
+            lastBricksDict = bricksDict
+            drawBrick(cm, bricksDict, brickD, key, loc, keys, i, dimensions, brickSize, split, customData, customObj_details, brickScale, keysNotChecked, bricksCreated, supportBrickDs, allBrickMeshes, logo, logo_details, mats, brick_mats, internalMat, randS1, randS2, randS3, randS4)
+
+            # print build status to terminal
+            old_percent = printBuildStatus(keys, printStatus, cursorStatus, keysNotChecked, old_percent)
+
+            # remove keys in new brick from keysNotChecked (for attemptMerge)
+            updateKeysNotChecked(brickSize, loc, zStep, keysNotChecked, key)
 
     # remove duplicate of original logoDetail
     if cm.logoDetail != "LEGO" and logo is not None:

functions/makeBricks_utils.py
@@ -143,10 +143,10 @@ def addEdgeSplitMod(obj):
     eMod = obj.modifiers.new('Edge Split', 'EDGE_SPLIT')
 
 
-def mergeWithAdjacentBricks(cm, brickD, bricksDict, key, keysNotChecked, loc, brickSizes, zStep, randS1):
+def mergeWithAdjacentBricks(cm, brickD, bricksDict, key, keysNotChecked, brickSizes, zStep, randS1):
     if brickD["size"] is None or (cm.buildIsDirty):
         preferLargest = brickD["val"] > 0 and brickD["val"] < 1
-        brickSize = attemptMerge(cm, bricksDict, key, keysNotChecked, loc, brickSizes, zStep, randS1, preferLargest=preferLargest, mergeVertical=True)
+        brickSize = attemptMerge(cm, bricksDict, key, keysNotChecked, brickSizes, zStep, randS1, preferLargest=preferLargest, mergeVertical=True)
     else:
         brickSize = brickD["size"]
     return brickSize

lib/bricksDict/modify.py
@@ -102,9 +102,12 @@ def updateBrickSizes(cm, bricksDict, key, keys, loc, brickSizes, zStep, maxL, me
         if breakOuter2: break
 
 
-def attemptMerge(cm, bricksDict, key, keys, loc, brickSizes, zStep, randState, preferLargest=False, mergeVertical=True, shortType="PLATE", tallType="BRICK", height3Only=False):
+def attemptMerge(cm, bricksDict, key, keys, brickSizes, zStep, randState, preferLargest=False, mergeVertical=True, shortType="PLATE", tallType="BRICK", height3Only=False):
     """ attempt to merge bricksDict[key] with adjacent bricks """
     assert len(brickSizes) > 0
+
+    # get loc from key
+    loc = strToList(key)
 
     if cm.brickType != "CUSTOM":
         # iterate through adjacent locs to find available brick sizes

commit 178153f823ab988f9bf456a8a1bb6efae7009f46

Fixed error introduced in commit 64ccf6f

buttons/customize/tools/mergeBricks.py
@@ -125,22 +125,21 @@ class mergeBricks(Operator):
         zStep = getZStep(cm)
         randState = np.random.RandomState(cm.mergeSeed)
 
-        # remove child bricks from keys list
-        keys = [k for k in keys if bricksDict[k]["parent_brick"] in [None, "self"]]
         # sort keys
         keys.sort(key=lambda k: (strToList(k)[0] * strToList(k)[1] * strToList(k)[2]))
 
         for key in keys:
-            # attempt to merge current brick with other bricks in keys, according to available brick types
-            parentD = bricksDict[getParentKey(bricksDict, key)]
-            tallType = targetType if targetType in getBrickTypes(height=3) else parentD["type"]
-            shortType = targetType if targetType in getBrickTypes(height=1) else (parentD["type"] if parentD["type"] in getBrickTypes(height=1) else "PLATE")
-            brickSize = attemptMerge(cm, bricksDict, key, keys, [parentD["size"]], zStep, randState, preferLargest=True, mergeVertical=mergeVertical, shortType=shortType, tallType=tallType, height3Only=height3Only)
-            # set exposure of current [merged] brick
-            topExposed, botExposed = getBrickExposure(cm, bricksDict, key)
-            bricksDict[key]["top_exposed"] = topExposed
-            bricksDict[key]["bot_exposed"] = botExposed
-            updatedKeys.append(key)
+            if bricksDict[key]["parent_brick"] in [None, "self"]:
+                # attempt to merge current brick with other bricks in keys, according to available brick types
+                parentD = bricksDict[getParentKey(bricksDict, key)]
+                tallType = targetType if targetType in getBrickTypes(height=3) else parentD["type"]
+                shortType = targetType if targetType in getBrickTypes(height=1) else (parentD["type"] if parentD["type"] in getBrickTypes(height=1) else "PLATE")
+                brickSize = attemptMerge(cm, bricksDict, key, keys, [parentD["size"]], zStep, randState, preferLargest=True, mergeVertical=mergeVertical, shortType=shortType, tallType=tallType, height3Only=height3Only)
+                # set exposure of current [merged] brick
+                topExposed, botExposed = getBrickExposure(cm, bricksDict, key)
+                bricksDict[key]["top_exposed"] = topExposed
+                bricksDict[key]["bot_exposed"] = botExposed
+                updatedKeys.append(key)
         return updatedKeys
 
     #############################################