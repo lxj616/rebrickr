95970d250ec906461eb9bb6f6bf0739606fb86f4
“bblanimation” <cgear13@gmail.com>
2018-03-01 14:57:23 -0500

    Pointed all URLs back to original rebrickr name

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ bl_info = {
     "description" : "Turn any mesh into a 3D brick sculpture or simulation with the click of a button",
     "location"    : "View3D > Tools > Bricker",
     "warning"     : "",  # used for warning icon and text in addons panel
-    "wiki_url"    : "https://www.blendermarket.com/creator/products/bricker/",
-    "tracker_url" : "https://github.com/bblanimation/bricker/issues",
+    "wiki_url"    : "https://www.blendermarket.com/creator/products/rebrickr/",
+    "tracker_url" : "https://github.com/bblanimation/rebrickr/issues",
     "category"    : "Object"}
 
 """
